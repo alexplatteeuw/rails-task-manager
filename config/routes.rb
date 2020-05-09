f508476a6d97423bdfba405def2421338de39f0f
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-05-09 14:19:12 +0200

    added root direction

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # get 'tasks/:id', to: 'tasks#show', as: :task
   # patch 'tasks/:id', to: 'tasks#update'
   # delete 'tasks/:id', to: 'tasks#destroy'
+  root 'tasks#index'
   resources :tasks
 end
